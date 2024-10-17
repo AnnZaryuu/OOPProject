4a0f99328a44139e4fdf36052fa56c20b1de7630
AnnZaryuu <ginanjarlaksana338@gmail.com>
2024-10-18 03:58:44 +0700

    Aggregation git push origin main

--- domain_object/node_role.php
+++ domain_object/node_role.php
@@ -1,16 +1,25 @@
 <?php
 
+//Aggregation
+class EmailUser{
+    Public $email;
+    public function __construct($email) { 
+        $this->email = $email; }
+}
+
 
 class Role {
     public $role_id;
     public $role_name;
     public $role_description;
     public $role_status;
+    public $email;
 
-    public function __construct($role_id, $role_name, $role_description, $role_status) {
+    public function __construct($role_id, $role_name, $role_description, $role_status,EmailUser $email_user) {
         $this->role_id = $role_id;
         $this->role_name = $role_name;
         $this->role_description = $role_description;
         $this->role_status = $role_status;
+        $this->email = $email_user;
     }
 }
